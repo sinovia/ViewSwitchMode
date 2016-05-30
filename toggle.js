26ed43837741a85e6b3f03a30eea04067feb9dff
SINOVIA <somayfield@gmail.com>
2016-05-30 19:29:21 -0400

    Both Style Sheets, Working on Toggle

---
+++ toggle.js
@@ -0,0 +1,12 @@
+document.getElementById("grid").addEventListener("click", function (){
+document.getElementById("stylesswitch")
+    if (stylesswitch.href == "style2.css") {
+        stylesswitch.href = "styles.css";
+    }
+    else {
+        stylesswitch.href = "styles.css";
+    }
+}
+
+
+//function toggle() {
